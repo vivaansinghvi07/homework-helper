7a164b4a8075ad9e980aff422f46ca368a6b38b7
Vivaan Singhvi <vivaansinghvi8@gmail.com>
2023-09-08 01:32:44 -0400

    updated api requests to handle cropping, OCR, and AI

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,17 @@
-from flask import Flask, request
-import flask
 import json
+import flask
+from utils import (
+    get_ai_response,
+    b64_request_to_img,
+    img_to_b64_response,
+    get_isolated_homework,
+    extract_text_linewise,
+)
 from flask_cors import CORS
+from flask import Flask, request, Response
 
+
+# setup app
 app = Flask(__name__)
 CORS(app)
 
@@ -10,6 +19,41 @@ CORS(app)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route('/api/homework_cropper', methods=["POST"])
+def homework_cropper() -> Response:
+    img = b64_request_to_img()
+    cropped_homework = get_isolated_homework(img)
+    return Response(response=img_to_b64_response(cropped_homework), status=200)
+
+
+@app.route('/api/read_text', methods=["POST"])
+def read_text() -> Response:
+    img = b64_request_to_img()
+    text = extract_text_linewise(img)
+    return Response(response=text, status=200)
+
+
+@app.route('/api/apply_ai', methods=["POST"])
+def apply_ai() -> Response:
+    data = request.get_json()
+    method = data["method"]
+    if method == "ANSWER":
+        prompt = """\
+        Answer the following question. If there are many answers, number your answers \
+        in the same way as the questions are numbered. Here is the question:
+        {}
+        """
+    elif method == "PRACTICE":
+        prompt = """\
+        Generate practice problems similar to the question below, where each problem \
+        is seperated by "\n---\n":
+        {}
+        """
+    else:
+        return Response(response="Bad prompt request", status=400)
+    return Response(response=get_ai_response(prompt, data["text"]), status=200)
 
 
 @app.route('/users', methods=["GET", "POST"])
@@ -32,7 +76,7 @@ def users():
             "status": "success",
             "message": f"received: {message}"
         }
-        return flask.Response(response=json.dumps(return_data), status=201)
+        return Response(response=json.dumps(return_data), status=201)
 
 
 if __name__ == "__main__":
---
+++ backend/utils.py
@@ -0,0 +1,117 @@
+import cv2
+import g4f
+import math
+import base64
+import numpy as np
+from flask import request
+from paddleocr import PaddleOCR
+
+OCR = PaddleOCR(use_angle_cls=True, lang='en', show_log=False, rotation=True)
+
+
+def b64_request_to_img() -> cv2.Mat:
+    received_image = request.get_json()
+    base64_image = received_image["data"]
+    np_buf = np.frombuffer(base64.b64decode(base64_image), np.uint8)
+    return cv2.imdecode(np_buf, cv2.IMREAD_COLOR)
+
+
+def img_to_b64_response(img: cv2.Mat) -> bytes:
+    return base64.b64encode(cv2.imencode('.jpg', img)[1])
+
+
+def get_isolated_homework(img: cv2.Mat):
+    """
+
+    Args:
+        img: A np.ndarray shaped (None, None, 3) containing image data focusing on a paper.
+
+    Returns: The image, with a warped perspective, focusing on the main paper in the picture.
+
+    """
+
+    # obtains the contour for the paper
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 6)
+    dilated = cv2.dilate(thresh, np.ones((3, 3)))
+    contours, _ = cv2.findContours(cv2.bitwise_not(dilated, np.full_like(img, 255)), cv2.RETR_TREE,
+                                   cv2.CHAIN_APPROX_SIMPLE)
+    paper = max(contours, key=lambda cnt: cv2.contourArea(cnt))
+
+    # obtains the four-point approximation for the paper
+    min_area_rect = cv2.boxPoints(cv2.minAreaRect(paper))
+    points = [None] * 4
+    dists = [100_000] * 4
+    for point in paper.reshape(-1, 2):
+        for i, (rect_point, stored, d) in enumerate(zip(min_area_rect, points, dists)):
+            if (new_dist := math.dist(point, rect_point)) < d:
+                dists[i] = new_dist
+                points[i] = [*point]
+
+    # performs the four-point transform
+    points = sorted(points, key=lambda x: x[1])
+    tl, tr = sorted(points[0:2], key=lambda x: x[0])
+    bl, br = sorted(points[2:4], key=lambda x: x[0])
+    max_height = int(max(math.dist(bl, tl), math.dist(br, tr)))
+    max_width = int(max(math.dist(bl, br), math.dist(tl, tr)))
+    dst = np.array([
+        [0, 0],
+        [max_width - 1, 0],
+        [max_width - 1, max_height - 1],
+        [0, max_height - 1]
+    ], dtype='float32')
+    M = cv2.getPerspectiveTransform(np.array([tl, tr, br, bl], dtype='float32'), dst)
+    warped = cv2.warpPerspective(img, M, (max_width, max_height))
+    return warped
+
+
+def _get_y(x: list[int, int]) -> int:
+    """
+
+    Args:
+        x: A list containing the coordinates for a two-dimensional point.
+
+    Returns: The y-coordinate of the point
+
+    """
+
+    return x[1]
+
+
+def extract_text_linewise(img: cv2.Mat) -> str:
+    """
+
+    Args:
+        img: The image from which to read text.
+
+    Returns: A string of interpreted text for the imag.
+
+    """
+
+    # obtains text and other information about the text
+    text = OCR.ocr(img)
+    boxes = [np.array(t[0], dtype=np.int32) for t in text[0]]
+    text_height = np.average(np.array([max(b) - min(b) for b in map(lambda x: list(map(_get_y, x)), boxes)]))
+    top_to_bottom = sorted(text[0], key=lambda x: min(map(_get_y, x[0])))
+
+    # determines whether to merge each line with the previous one
+    merge_with_prev = [max(map(_get_y, prev[0])) - min(map(_get_y, line[0])) > text_height // 2 for prev, line in
+                       zip(top_to_bottom, top_to_bottom[1:])]
+
+    # builds final string
+    final_string = ""
+    for merge, [_, (line, _)] in zip([True] + merge_with_prev, top_to_bottom):
+        if merge:
+            final_string += f"{line} "
+        else:
+            final_string += f"\n{line} "
+    return final_string
+
+
+def get_ai_response(prompt: str, problem: str) -> str:
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": prompt.format(problem)}],
+        provider=g4f.Provider.DeepAi
+    )
+    return response
